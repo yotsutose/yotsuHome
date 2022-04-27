3c40677c9eaac400b94c92888a7f366525ae8d58
yotsutose <becchi0424@gmail.com>
2022-04-28 02:27:35 +0900

    github page用にexport

---
+++ out/_next/static/zCualfU6wYvPGikZDoszF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/16b86961c0e437ddb637.css","static/chunks/pages/index-438379d44017f05951c3.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
