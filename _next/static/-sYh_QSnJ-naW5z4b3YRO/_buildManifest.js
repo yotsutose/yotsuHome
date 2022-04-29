cc69d358eb7111b22a5efe140d002e4dd17f3a42
yotsutose <yotsutose@users.noreply.github.com>
2022-04-29 08:49:11 +0000

    deploy: 0d87f49392075c43fbef182ecedd0f357e4cc15e

--- _next/static/-sYh_QSnJ-naW5z4b3YRO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/38116b30d19c0987d116.css","static/chunks/pages/index-625d50d48931af2e4ecf.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
