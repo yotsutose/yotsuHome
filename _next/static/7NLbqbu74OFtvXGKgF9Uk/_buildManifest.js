0d7ce6a20d0d2c039f89583d5b3024e93f284ca8
yotsutose <yotsutose@users.noreply.github.com>
2022-04-30 04:55:38 +0000

    deploy: 1530ebd9f539202f0fbc021e6c2aa802cd304c70

--- _next/static/7NLbqbu74OFtvXGKgF9Uk/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/332053fb4065d4f49194.css","static/chunks/pages/index-3e861d4fb4ff7303fd2e.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/LminY2WiX7QkPhel7zKyT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/332053fb4065d4f49194.css","static/chunks/pages/index-acc4b45dfcfe88f220cb.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
